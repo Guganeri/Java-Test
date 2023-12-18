6e1172ac586560ea1741da040dc2d70d7e2b6048
Guga Neri <guganeriss@gmail.com>
2023-12-18 16:35:26 +0000

    first commit

---
+++ java_completo/src/java_completo/Main.java
@@ -0,0 +1,26 @@
+package java_completo;
+import java.util.Locale;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá Mundo!");
+		System.out.println("Bom dia!");
+		
+		int y = 32;
+		double x = 10.35784;
+		
+		System.out.println(y);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f\n", x);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f\n", x);
+		
+		System.out.println("RESULTADO = "+ x + " METROS");
+		System.out.printf("RESULTADO = %.2f metros \n", x);
+
+	}
+
+}
