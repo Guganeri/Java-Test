9a5ee9dc4a9873a96a11c871d20866656d035311
Guga Neri <guganeriss@gmail.com>
2024-01-12 11:55:20 +0000

    otimyzer use length

--- exercicioVector2/src/application/Program.java
+++ exercicioVector2/src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		int n = sc.nextInt();
 		Product[] vect = new Product[n];
 		
-		for (int i=0; i<n; i++) {
+		for (int i=0; i<vect.length; i++) {
 			sc.next();
 			String name = sc.nextLine();
 			double price = sc.nextDouble();
@@ -23,10 +23,10 @@ public class Program {
 		}
 		
 		double sum = 0.0;
-		for (int i=0; i<n; i++) {
+		for (int i=0; i<vect.length; i++) {
 			sum += vect[i].getPrice();
 		}
-		double avg = sum / n;
+		double avg = sum / vect.length;
 		
 		System.out.printf("AVERAEGE PRICE = %.2f", avg);
 				
