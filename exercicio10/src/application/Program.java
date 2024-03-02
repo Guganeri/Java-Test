2979276a2e36fccc755c0327993ca74a29686dc4
Gustavo Neri <guganeriss@gmail.com>
2024-03-02 00:30:21 +0000

    aulao

--- exercicio10/src/application/Program.java
+++ exercicio10/src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 import entities.OrderItem;
 import entities.Product;
 
@@ -7,9 +10,19 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
 		Product p = new Product("Tv", 1000.0);
 		
-		OrderItem oi1 = new OrderItem(null, 0, p);
+		OrderItem oi1 = new OrderItem(1, 1000, p);
+		
+		System.out.println(oi1.getProduct().getName());
+		
+		System.out.println(oi1);
+		
+		
+		sc.close();
 		
 	}
 
